d396e8c55dd60622ddfb31b0ad8bb37a78c431f2
Mike Walker <mike@incredimike.com>
2023-07-26 02:39:57 -0700

    Remove constructor from TransactionCollection

--- app/Budget/TransactionCollection.php
+++ app/Budget/TransactionCollection.php
@@ -6,17 +6,11 @@ use Illuminate\Support\Collection;
 
 class TransactionCollection extends Collection
 {
-    public function __construct($items = [])
-    {
-        parent::__construct($items);
-    }
-
     public function flattenTransactions(): TransactionCollection
     {
         [$transactions, $hasSubtransactions] = $this->partition(
             fn(array $transaction) => empty($transaction['subtransactions'])
         );
-
         $hasSubtransactions->each(function ($transaction) use ($transactions) {
             foreach ($transaction['subtransactions'] as $sub) {
                 $new = [...$transaction, ...$sub];
