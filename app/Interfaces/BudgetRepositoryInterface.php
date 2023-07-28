22605082664d31aa926b50b8a2d4fe8574eded04
Mike Walker <mike@incredimike.com>
2023-07-28 02:08:05 -0700

    Add getTransaction() to BudgetRepository

--- app/Interfaces/BudgetRepositoryInterface.php
+++ app/Interfaces/BudgetRepositoryInterface.php
@@ -2,8 +2,11 @@
 
 namespace App\Interfaces;
 
+use App\DTOs\TransactionDTO;
+
 interface BudgetRepositoryInterface
 {
     public function getServiceName(): string;
     public function getTransactionsSince(string $date): array;
+    public function getTransaction(string $id): array;
 }
--- app/Repositories/YnabBudgetRepository.php
+++ app/Repositories/YnabBudgetRepository.php
@@ -6,25 +6,45 @@ use App\Interfaces\BudgetRepositoryInterface;
 
 class YnabBudgetRepository extends BudgetRepository implements BudgetRepositoryInterface
 {
-    protected string $serviceUrl = 'https://api.ynab.com/v1';
-    protected string $serviceName = 'YNAB';
     protected string $budgetId = 'last-used';
+    protected string $serviceName = 'YNAB';
+    protected string $serviceUrl = 'https://api.ynab.com/v1';
 
     public function getServiceName(): string
     {
         return $this->serviceName;
     }
 
-    public function getTransactionsSince(string $sinceDate): array
+    public function getTransactionsSince(string $date): array
     {
         $base_url = sprintf(
             $this->serviceUrl .  '/budgets/%s/transactions',
             $this->budgetId
         );
         $query_params = http_build_query([
-            'since_date' => $sinceDate,
+            'since_date' => $date,
         ]);
 
         return $this->fetchJson($base_url . '?' . $query_params, 'data.transactions');
+    }
+
+    public function getTransaction(string $id): array
+    {
+        $url = sprintf(
+            $this->serviceUrl . '/budgets/%s/transactions/%s',
+            $this->budgetId,
+            $id
+        );
+        return $this->fetchJson($url, 'data.transaction');
+    }
+
+    public function getBudgetId(): string
+    {
+        return $this->budgetId;
+    }
+
+    public function setBudgetId(string $budgetId): void
+    {
+        $this->budgetId = $budgetId;
     }
 }
